d1b3511ce4247175b291d722565e4a58719b8fd0
Federico Capaldo <federico.capaldo@hotmail.com>
2024-11-24 13:55:22 +0000

    changed content method to save content to database (left in error that wont update recipe upon modifying the existing ingredients/name of recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,15 +1,27 @@
 class Recipe < ApplicationRecord
 
-
-
   def content
-    Rails.cache.fetch("#{cache_key_with_version}/content") do
-      client = OpenAI::Client.new
-      chatgpt_response = client.chat(parameters: {
-        model: "gpt-4o-mini",
-        messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
-      })
-      chatgpt_response["choices"][0]["message"]["content"]
+    if super.blank?
+      set_content
+    else
+      super
     end
+  end
+
+
+
+  private
+
+  def set_content
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+    })
+    new_content = chatgpt_response["choices"][0]["message"]["content"]
+
+    update(content: new_content)
+
+    return new_content
   end
 end
