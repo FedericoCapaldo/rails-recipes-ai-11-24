2cc4bfb65a371cf47011a534bb597e3ebad950d6
Federico Capaldo <federico.capaldo@hotmail.com>
2024-11-24 12:24:35 +0000

    integrated openai request for recipe into our controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,12 @@ class RecipesController < ApplicationController
 
   # GET /recipes/1
   def show
+    client = OpenAI::Client.new
+    chatgpt_response = client.chat(parameters: {
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: "Give me a simple recipe for #{@recipe.name} with the ingredients #{@recipe.ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+    })
+    @content = chatgpt_response["choices"][0]["message"]["content"]
   end
 
   # GET /recipes/new
