ef1ec9953fda63a0067be4f197d60c260a184e7d
Federico Capaldo <federico.capaldo@hotmail.com>
2024-11-24 13:42:47 +0000

    added simple caching with rails internal cache for the content to generate a recipe only once every update of the recipe instead of every reload of the show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,11 +3,13 @@ class Recipe < ApplicationRecord
 
 
   def content
-    client = OpenAI::Client.new
-    chatgpt_response = client.chat(parameters: {
-      model: "gpt-4o-mini",
-      messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
-    })
-    return chatgpt_response["choices"][0]["message"]["content"]
+    Rails.cache.fetch("#{cache_key_with_version}/content") do
+      client = OpenAI::Client.new
+      chatgpt_response = client.chat(parameters: {
+        model: "gpt-4o-mini",
+        messages: [{ role: "user", content: "Give me a simple recipe for #{name} with the ingredients #{ingredients}. Give me only the text of the recipe, without any of your own answer like 'Here is a simple recipe'."}]
+      })
+      chatgpt_response["choices"][0]["message"]["content"]
+    end
   end
 end
